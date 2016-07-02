f4f6f0bf52de54544b58d0e9805206ad4d405874
David Westreicher <david.westreicher@gmail.com>
2016-07-02 04:05:04 +0200

    trying to fix disconnect bug

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ var channelConfig = {
 
 var peers = {}
 var state = {}
+var toremove = []
 
 var time = 0;
 var messagebuf = new Int16Array(10*2); // 10 players
@@ -22,13 +23,21 @@ var broadcaster = setInterval(function(){
     //console.log(Object.keys(peers).length);
     //TODO peers access should be secured with mutex
     //console.log(state);
+    for(var toremoveid in toremove){
+        delete peers[toremoveid];
+        delete state[toremoveid];
+        console.log('removing id: '+ toremoveid);
+        console.log('resulting state: ');
+        console.log(state);
+        console.log(Object.keys(state));
+    }
     var players = Object.keys(state).length;
     var index = 0;
-    for(var id in Object.keys(state)){
+    Object.keys(state).forEach(function(id){
         messagebuf[index*2+0] = state[id][0];
         messagebuf[index*2+1] = state[id][1];
         index++;
-    }
+    });
     for(var c in Object.keys(peers)){
         var peer = peers[c];
         if(peer && peer.connected)
@@ -40,48 +49,43 @@ var broadcaster = setInterval(function(){
 
 wss.on('connection', function connection(ws) {
     //TODO generate random id
-    //TODO peers access should be secured with mutex
     var id = Object.keys(peers).length;
     var peer = new Peer({ initiator: true, wrtc: wrtc, channelConfig:channelConfig, offerConstraints:{}})
     peer.on('signal', function (data) {
-        console.log('PEER signal')
-        //console.log(data)
+        console.log('PEER '+id+' signal: '+data)
         ws.send(JSON.stringify(data));
     })
     var interval = null;
     peer.on('connect', function(){
-        console.log('PEER connected');
+        console.log('PEER '+id+' connected');
         peers[id] = peer;
         state[id] = [0,0];
     });
     peer.on('data', function(data){
-        /*
-        console.log('PEER data');
-        console.log(data);
-        console.log(typeof data);
-        console.log(data.length);
-        */
-        state[id][0] = data.readInt16LE(0);
-        state[id][1] = data.readInt16LE(2);
+        if(typeof state[id] != 'undefined'){
+            state[id][0] = data.readInt16LE(0);
+            state[id][1] = data.readInt16LE(2);
+        }else{
+            console.log('ondataexit')
+            console.log(data)
+        }
     });
     peer.on('error', function(err){
-        console.log('PEER onerror');
+        console.log('PEER '+id+' onerror: '+err);
         console.log(err);
-        delete peers[peer];
-        delete peers[id];
+        toremove.push(id);
     });
     peer.on('close', function(){
-        console.log('PEER onclose');
-        delete peers[peer];
-        delete peers[id];
+        console.log('PEER '+id+' onclose');
+        toremove.push(id);
     });
     ws.on('message', function(message) {
-        console.log('WEBSOCKET onmessage');
+        console.log('WEBSOCKET '+id+' onmessage: '+message);
         var data = JSON.parse(message);
         peer.signal(data);
     });
     ws.on('close', function close() {
-        console.log('WEBSOCKET disconnected');
+        console.log('WEBSOCKET '+id+' disconnected');
     });
 });
 
