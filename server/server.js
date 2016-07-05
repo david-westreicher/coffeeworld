1331775e4ad165c1c31681ae8b671e1bd82c2b26
David Westreicher <david.westreicher@gmail.com>
2016-07-05 19:58:44 +0200

    use ecma6 syntax for server

--- server/server.js
+++ server/server.js
@@ -1,91 +1,96 @@
-var Peer = require('simple-peer');
-var wrtc = require('wrtc');
-var WebSocketServer = require('ws').Server;
-var PORT = 9000;
-var TICKRATE = 16;
+'use strict'
 
-var wss = new WebSocketServer({ port: PORT });
-console.log('Starting websocketserver on port: ' + PORT);
+const PORT = 9000
+const TICKRATE = 50
+const Peer = require('simple-peer')
+const wrtc = require('wrtc')
+const WebSocketServer = require('ws').Server
 
-var channelConfig = {
+const wss = new WebSocketServer({ port: PORT })
+console.log('Starting websocketserver on port: ' + PORT)
+
+
+const peers = {}
+const state = {}
+const toremove = []
+
+const messagebuf = new Int16Array(10*2) // 10 players
+setInterval(() => {
+    //console.log(peers)
+    //console.log(Object.keys(peers).length)
+    //TODO peers access should be secured with mutex
+    //console.log(state)
+    for(const toremoveid in toremove){
+        delete peers[toremoveid]
+        delete state[toremoveid]
+        console.log('removing id: '+ toremoveid)
+        console.log('resulting state: ')
+        console.log(state)
+        console.log(Object.keys(state))
+    }
+    const players = Object.keys(state).length
+    let index = 0
+    Object.keys(state).forEach(function(id){
+        messagebuf[index*2+0] = state[id][0]
+        messagebuf[index*2+1] = state[id][1]
+        index++
+    })
+    for(const c in Object.keys(peers)){
+        const peer = peers[c]
+        if(peer && peer.connected)
+            //TODO use reusable buffer
+            peer.send(messagebuf.slice(0,players*2))
+    }
+}, TICKRATE)
+
+
+const channelConfig = {
     ordererd: false,
     maxRetransmits: 0
 }
-
-var peers = {}
-var state = {}
-var toremove = []
-
-var time = 0;
-var messagebuf = new Int16Array(10*2); // 10 players
-var broadcaster = setInterval(function(){
-    //console.log(peers);
-    //console.log(Object.keys(peers).length);
-    //TODO peers access should be secured with mutex
-    //console.log(state);
-    for(var toremoveid in toremove){
-        delete peers[toremoveid];
-        delete state[toremoveid];
-        console.log('removing id: '+ toremoveid);
-        console.log('resulting state: ');
-        console.log(state);
-        console.log(Object.keys(state));
-    }
-    var players = Object.keys(state).length;
-    var index = 0;
-    Object.keys(state).forEach(function(id){
-        messagebuf[index*2+0] = state[id][0];
-        messagebuf[index*2+1] = state[id][1];
-        index++;
-    });
-    for(var c in Object.keys(peers)){
-        var peer = peers[c];
-        if(peer && peer.connected)
-            //TODO use reusable buffer
-            peer.send(messagebuf.slice(0,players*2));
-    }
-    time++;
-}, TICKRATE);
-
-wss.on('connection', function connection(ws) {
+wss.on('connection', (ws) => {
     //TODO generate random id
-    var id = Object.keys(peers).length;
-    var peer = new Peer({ initiator: true, wrtc: wrtc, channelConfig:channelConfig, offerConstraints:{}})
-    peer.on('signal', function (data) {
-        console.log('PEER '+id+' signal: '+data)
-        ws.send(JSON.stringify(data));
+    const id = Object.keys(peers).length
+    const peer = new Peer({
+        initiator: true,
+        wrtc: wrtc,
+        channelConfig:channelConfig,
+        offerConstraints:{}
     })
-    var interval = null;
-    peer.on('connect', function(){
-        console.log('PEER '+id+' connected');
-        peers[id] = peer;
-        state[id] = [0,0];
-    });
-    peer.on('data', function(data){
+    peer.on('signal', (data) => {
+        console.log('PEER '+id+' signal: '+data)
+        ws.send(JSON.stringify(data))
+    })
+    peer.on('connect', () => {
+        console.log('PEER '+id+' connected')
+        peers[id] = peer
+        state[id] = [0,0]
+    })
+    peer.on('data', (data) => {
         if(typeof state[id] != 'undefined'){
-            state[id][0] = data.readInt16LE(0);
-            state[id][1] = data.readInt16LE(2);
+            state[id][0] = data.readInt16LE(0)
+            state[id][1] = data.readInt16LE(2)
         }else{
             console.log('ondataexit')
             console.log(data)
         }
-    });
-    peer.on('error', function(err){
-        console.log('PEER '+id+' onerror: '+err);
-        console.log(err);
-        toremove.push(id);
-    });
-    peer.on('close', function(){
-        console.log('PEER '+id+' onclose');
-        toremove.push(id);
-    });
-    ws.on('message', function(message) {
-        console.log('WEBSOCKET '+id+' onmessage: '+message);
-        var data = JSON.parse(message);
-        peer.signal(data);
-    });
-    ws.on('close', function close() {
-        console.log('WEBSOCKET '+id+' disconnected');
-    });
-});
+    })
+    peer.on('error', (err) => {
+        console.log('PEER '+id+' onerror: '+err)
+        console.log(err)
+        toremove.push(id)
+    })
+    peer.on('close', () => {
+        console.log('PEER '+id+' onclose')
+        toremove.push(id)
+    })
+    ws.on('message', (message) => {
+        console.log('WEBSOCKET '+id+' onmessage: '+message)
+        const data = JSON.parse(message)
+        peer.signal(data)
+    })
+    ws.on('close', () => {
+        console.log('WEBSOCKET '+id+' disconnected')
+    })
+})
 
